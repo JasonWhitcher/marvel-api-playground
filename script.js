2a41c100a46b2792aa79c38ea2144309d313d86c
Jason Whitcher <thejasonwhticher@gmail.com>
2020-03-03 16:04:26 -0600

    changes

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 window.onload = () => {
-    fetch('http://gateway.marvel.com/v1/public/characters/1?apikey=b703ebcf36f7b7bdb42b10f2dd8f1b39')
+    fetch('http://gateway.marvel.com/v1/public/characters/12345?apikey=b703ebcf36f7b7bdb42b10f2dd8f1b39')
         .then( (response) => {
             return response.json();
         })
