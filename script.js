e34fdc4a5008db1627fd2f2e8e01b0beeed75fbd
JasonWhitcher <jasonwh@hotmail.com>
2020-03-09 12:52:29 -0500

    changes

--- script.js
+++ script.js
@@ -1,5 +1,28 @@
+
+let url = 'https://gateway.marvel.com/v1/public/characters';
+let apiKey = 'apikey=b703ebcf36f7b7bdb42b10f2dd8f1b39';
+let paramNameStartsWith = 'nameStartsWith=';
+let paramLimitNumber = 'limit=' + 5;
+
 window.onload = () => {
-    fetch('https://gateway.marvel.com/v1/public/characters/1016823?apikey=b703ebcf36f7b7bdb42b10f2dd8f1b39')
+    let inputName = document.getElementById('chr-name');
+    
+    inputName.addEventListener('input', (event) => {
+        let chrName = inputName.value;
+        console.log(chrName);
+        fetch(url+ '?' + paramNameStartsWith + chrName + '&' + paramLimitNumber + '&' + apiKey)
+            .then( (response) => {
+                return response.json();
+            })
+            .then( (data) => {
+                console.log(data);
+            })
+            .catch( (error) => {
+                console.log('Oh No:' + error);
+            });
+    });
+    
+    /*fetch('https://gateway.marvel.com/v1/public/characters/1016823?apikey=b703ebcf36f7b7bdb42b10f2dd8f1b39')
         .then( (response) => {
             return response.json();
         })
@@ -8,5 +31,5 @@ window.onload = () => {
         })
         .catch( (error) => {
             console.log('Oh No:' + error);
-        });
+        });*/
 };
