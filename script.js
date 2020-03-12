a2895646b98353a7b0d5cf99bc9f260753fb0770
JasonWhitcher <jasonwh@hotmail.com>
2020-03-12 11:26:18 -0500

    changes

--- script.js
+++ script.js
@@ -10,31 +10,27 @@ window.onload = () => {
 
     inputName.addEventListener('input', (event) => {
         let chrName = inputName.value;
-        console.log(chrName);
-        fetch(url+ '?' + paramNameStartsWith + chrName + '&' + paramLimitNumber + '&' + apiKey)
-            .then( (response) => {
-                return response.json();
-            })
-            .then( (data) => {
-                let names = data.data.results;
-                let optionsList = '';
-                datalistElement.innerHTML = '';
-                for (let name of names) {
-                    optionsList += '<option value="' + name.name + '">';
-                    console.log(name.name);
-                }
-                console.log(optionsList);
-                setTimeout( () => { datalistElement.innerHTML = optionsList; }, 100);
-                //datalistElement.innerHTML = optionsList;
-                /*return optionsList;
-            })
-            .then( (optionsList) => {
-                console.log('TEST');
-                //datalistElement.innerHTML = optionsList;*/
-            })
-            .catch( (error) => {
-                console.log('Oh No:' + error);
-            });
+        console.log('charName: ' + chrName);
+        if (chrName.length < 0) {
+            fetch(url+ '?' + paramNameStartsWith + chrName + '&' + paramLimitNumber + '&' + apiKey)
+                .then( (response) => {
+                    return response.json();
+                })
+                .then( (data) => {
+                    let names = data.data.results;
+                    let optionsList = '';
+                    datalistElement.innerHTML = '';
+                    for (let name of names) {
+                        optionsList += '<option value="' + name.name + '">';
+                        console.log(name.name);
+                    }
+                    console.log(optionsList);
+                    datalistElement.innerHTML = optionsList;
+                })
+                .catch( (error) => {
+                    console.log('Oh No:' + error);
+                });
+        }
     });
     
     /*fetch('https://gateway.marvel.com/v1/public/characters/1016823?apikey=b703ebcf36f7b7bdb42b10f2dd8f1b39')
